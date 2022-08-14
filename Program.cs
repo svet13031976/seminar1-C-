88510e4753ef351e4373620817f53d8b6dfd8a9a
Sveta Semenova <svet13031976@yandex.com>
2022-08-14 15:34:14 +0300

    ДЗ к семинару 1 все задачи

--- Program.cs
+++ Program.cs
@@ -51,3 +51,21 @@ else
     Console.Write($"Число {number} нечетное");
  
 */
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+int current = 1;
+
+while (current <= number)
+{
+    if (current % 2 == 0)
+        Console.Write(current + " ");
+current++;
+}
+ 
+ */
+
+ 
